d3ffca2a1e74971c9ddc84939f1cf4ce94588a02
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-04 15:32:29 -0500

    r3f starter

---
+++ r3f-starter/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
