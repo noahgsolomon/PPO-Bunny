9f7cbc7dd40d27204a8f65ced9f70762826c4ed8
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-06 22:22:33 -0500

    Refactor Lights component and update global CSS styles

--- r3f-starter/app/game/Lights.tsx
+++ r3f-starter/app/game/Lights.tsx
@@ -5,19 +5,7 @@ import { DirectionalLightHelper } from 'three'
 export default function Lights() {
   return (
     <>
-      <directionalLight
-        castShadow
-        intensity={2.5}
-        shadow-mapSize={[1024, 1024]}
-        shadow-camera-near={1}
-        shadow-camera-far={100}
-        shadow-camera-top={10}
-        shadow-camera-right={10}
-        shadow-camera-bottom={-10}
-        shadow-camera-left={-10}
-        position={[-10, 5, 0]}
-        color={'#ffffff'}
-      />
+      <directionalLight intensity={2.5} position={[-10, 5, 0]} color={'#ffffff'} />
       <ambientLight intensity={1.5} />
     </>
   )
