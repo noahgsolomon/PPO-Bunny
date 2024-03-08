8a95c59ffd670edbba073bf25496d79360b9dc4c
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-08 12:44:37 -0500

    Add presentation controls and UI elements

--- r3f-starter/app/game/page.tsx
+++ r3f-starter/app/game/page.tsx
@@ -4,8 +4,10 @@ import Tiles from './Tiles'
 import dynamic from 'next/dynamic'
 import Lights from './Lights'
 import { Vector3 } from 'three'
-import { Html, PerspectiveCamera } from '@react-three/drei'
+import { Html, OrbitControls, PerspectiveCamera, PresentationControls } from '@react-three/drei'
 import { Button } from '@/components/ui/button'
+import { ArrowLeft, ArrowRight, Info } from 'lucide-react'
+import { config } from '@react-spring/three'
 
 const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), { ssr: false })
 const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
@@ -13,12 +15,40 @@ const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mo
 export default function Page() {
   return (
     <div>
-      <View orbit className='absolute top-0 w-full h-full'>
+      <View className='absolute top-0 w-full h-full touch-none'>
         {/* <fog attach='fog' args={['#121316', 0.1, 30]} /> */}
-        <Tiles />
+        <PresentationControls
+          enabled
+          global
+          cursor={false}
+          speed={1}
+          zoom={1}
+          rotation={[Math.PI * 0.175, 0, 0]}
+          polar={[0, Math.PI / 2]}
+          azimuth={[-Infinity, Infinity]}
+          config={config.slow}
+        >
+          <Tiles />
+        </PresentationControls>
+
         <Lights />
-        <PerspectiveCamera makeDefault position={[4, 10, 20]} />
+        <PerspectiveCamera makeDefault position={[0, 0, 25]} />
       </View>
+      <div className='z-10 absolute top-8 text-center w-full flex items-center flex-col gap-4'>
+        <h1 className=' font-bold italic text-4xl'>Bunny Bananza</h1>
+        <div className='flex flex-row gap-2'>
+          <Button size='lg'>Play</Button>
+          <Button size='lg' className='flex flex-row gap-2 ' variant='outline'>
+            Info <Info className='w-4 h-4' />
+          </Button>
+        </div>
+      </div>
+      <Button disabled variant='none' className='z-10 absolute right-10 top-1/2 transform -translate-y-1/2'>
+        <ArrowRight className='w-10 h-10' />
+      </Button>
+      <Button disabled variant='none' className='z-10 absolute left-10 top-1/2 transform -translate-y-1/2'>
+        <ArrowLeft className='w-10 h-10' />
+      </Button>
     </div>
   )
 }
