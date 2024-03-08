579edeaceecbf31163a033ef89749eb7b3a332ca
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-08 12:44:33 -0500

    water shading

--- r3f-starter/src/components/canvas/Scene.tsx
+++ r3f-starter/src/components/canvas/Scene.tsx
@@ -8,7 +8,7 @@ import { ACESFilmicToneMapping } from 'three'
 export default function Scene({ ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props} onCreated={(state) => (state.gl.toneMapping = ACESFilmicToneMapping)}>
+    <Canvas shadows {...props} onCreated={(state) => (state.gl.toneMapping = ACESFilmicToneMapping)}>
       <r3f.Out />
       <Preload all />
     </Canvas>
