df3913ad15848f25cb8252c8771a02416210a486
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-08 12:44:11 -0500

    shadows

--- r3f-starter/app/game/Lights.tsx
+++ r3f-starter/app/game/Lights.tsx
@@ -1,11 +1,19 @@
-import { useHelper } from '@react-three/drei'
-import { useRef } from 'react'
-import { DirectionalLightHelper } from 'three'
-
 export default function Lights() {
   return (
     <>
-      <directionalLight intensity={3} position={[-10, 5, 0]} color={'#ffffff'} />
+      <directionalLight
+        castShadow
+        intensity={2}
+        position={[-4, 5, 0]}
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={100}
+        shadow-camera-top={10}
+        shadow-camera-right={10}
+        shadow-camera-bottom={-10}
+        shadow-camera-left={-10}
+        color={'#ffffff'}
+      />
       <ambientLight intensity={1.5} />
     </>
   )
