7086d7f993bcbf90b797e9390cc62319dca7fa7a
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-11 11:46:29 -0400

    clone responds to its position

--- r3f-starter/app/game/Models/Clone.tsx
+++ r3f-starter/app/game/Models/Clone.tsx
@@ -1,15 +1,37 @@
+'use client'
+
 import { useCursor } from '@react-three/drei'
 import { forwardRef, useState } from 'react'
 import CloneBunny from '../CloneBunny'
+import { GroupProps } from '@react-three/fiber'
+import useEnvironment from '../store/useEnvironment'
 
-export const Clone = forwardRef<any, any>((props, ref) => {
+interface CloneProps extends GroupProps {
+  i: number
+}
+
+export const Clone = forwardRef<any, CloneProps>(({ i, ...groupProps }, ref) => {
   const [hovered, setHovered] = useState(false)
 
   useCursor(hovered)
 
+  const environment = useEnvironment()
+
+  const { x, y } = {
+    x: i % Math.sqrt(environment.TILE_COUNT),
+    y: Math.floor(i / Math.sqrt(environment.TILE_COUNT)),
+  }
+
   return (
-    <group ref={ref} {...props}>
+    <group ref={ref} {...groupProps}>
       <group
+        onClick={() => {
+          const agent = environment.agentEnvironment.filter(
+            (agent) => agent.position.x === x && agent.position.y === y,
+          )[0]
+
+          environment.setCurrentAgentIdx(agent.index)
+        }}
         onPointerEnter={(e) => {
           e.stopPropagation()
           setHovered(true)
