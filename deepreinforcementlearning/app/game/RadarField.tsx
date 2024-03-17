19d3a5cb22c59ab20a36dfbfa80742d62ca7f145
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-17 15:10:38 -0400

    moving radar

--- deepreinforcementlearning/app/game/RadarField.tsx
+++ deepreinforcementlearning/app/game/RadarField.tsx
@@ -1,12 +1,14 @@
+import { animated } from '@react-spring/three'
 import { RoundedBox } from '@react-three/drei'
+import { MeshProps } from '@react-three/fiber'
 
-export default function RadarField({ viewDistance = 5 }) {
-  const view = viewDistance * (5.5 / 5)
+export default function RadarField(props: MeshProps & { viewDistance: number }) {
+  const view = props.viewDistance * (5.5 / 5)
   return (
-    <mesh position-y={0.02} rotation-x={Math.PI * -0.5}>
+    <animated.mesh {...props} position-y={0.02} rotation-x={Math.PI * -0.5}>
       <RoundedBox args={[view, view, 0.1]}>
         <meshStandardMaterial transparent opacity={0.2} color='#00ff00' />
       </RoundedBox>
-    </mesh>
+    </animated.mesh>
   )
 }
