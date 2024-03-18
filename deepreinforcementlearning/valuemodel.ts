9c7e489fee554add736e7cce48d7a4fcdd2dea5c
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-18 17:11:39 -0400

    simpler networks

--- deepreinforcementlearning/valuemodel.ts
+++ deepreinforcementlearning/valuemodel.ts
@@ -2,7 +2,7 @@ import * as tf from '@tensorflow/tfjs-node'
 
 const model = tf.sequential()
 
-model.add(tf.layers.dense({ inputShape: [402], units: 64, activation: 'tanh' }))
+model.add(tf.layers.dense({ inputShape: [14], units: 64, activation: 'tanh' }))
 model.add(tf.layers.dense({ units: 1 }))
 
 model.save('file:///Users/noahs/Desktop/three/deepreinforcementlearning/value')
