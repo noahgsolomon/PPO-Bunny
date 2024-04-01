0c2d14d694b229a832ac7a1563b194077f5681f1
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-01 12:00:06 -0400

    scaling down

--- train/Agent.py
+++ train/Agent.py
@@ -1,18 +1,15 @@
 import random
 
-HEIGHT = 510
-WIDTH = 1005
+HEIGHT = 25
+WIDTH = 25
 
 class Snake:
 
     def __init__(self):
 
-        x = random.randint(15, WIDTH-30)
-        x -= x % 15 or 0
-        #x = (lambda num: num - num % 15 or 1)(random.randint(100, 1000))
+        x = random.randint(0, WIDTH-1)
 
-        y = random.randint(100, HEIGHT-30)
-        y -= y % 15 or 0
+        y = random.randint(0, HEIGHT-1)
 
         self.head = [x,y]
         self.tail = [[x,y]]
