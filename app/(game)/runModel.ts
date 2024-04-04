78717129b66d24bbba8fd6768e577ccada5ea84a
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-04 06:13:50 -0400

    run model works w/ diff input sizes

--- app/(game)/runModel.ts
+++ app/(game)/runModel.ts
@@ -18,15 +18,13 @@ export async function createModelCpu(model: ArrayBuffer): Promise<InferenceSessi
   })
 }
 
-export async function warmupModel(model: InferenceSession) {
+export async function warmupModel(model: InferenceSession, inputSize: number) {
   // OK. we generate a random input and call Session.run() as a warmup query
-  const warmupTensor = new Tensor('float32', new Float32Array(5), [1, 5])
+  const warmupTensor = new Tensor('float32', new Float32Array(inputSize), [1, inputSize])
 
-  warmupTensor.data[0] = 0
-  warmupTensor.data[1] = 0
-  warmupTensor.data[2] = 0
-  warmupTensor.data[3] = -4
-  warmupTensor.data[4] = 4
+  for (let i = 0; i < inputSize; i++) {
+    warmupTensor.data[i] = 0
+  }
 
   try {
     const feeds: Record<string, Tensor> = {}
@@ -37,13 +35,13 @@ export async function warmupModel(model: InferenceSession) {
   }
 }
 
-export async function runModel(model: InferenceSession, input: number[][]) {
+export async function runModel(model: InferenceSession, input: number[][], inputSize: number) {
   const start = new Date()
   const outputs: number[] = []
   const inferenceTimes: number[] = []
 
   for (const singleInput of input) {
-    const tensor = new Tensor('float32', new Float32Array(singleInput), [1, 5])
+    const tensor = new Tensor('float32', new Float32Array(singleInput), [1, inputSize])
 
     try {
       const feeds: Record<string, Tensor> = {}
